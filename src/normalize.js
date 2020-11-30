4cabd1ed6a5e297f43a1400f75d75f77b300c352
kangyeop <lky5697@naver.com>
2020-11-30 21:18:18 +0900

    feat: Remove unnecessary thing

--- src/normalize.js
+++ src/normalize.js
@@ -4,13 +4,9 @@ document.querySelector("#editor").addEventListener("keyup", () => {
 
 export const normalize = () => {
     const text = document.querySelector("#editor").value;
-    const hasCarriage = /\r\n?/g;
     const tooManyNewLine = /\n+$/g;
 
-    const inputLines = text
-        .replace(hasCarriage, "\n")
-        .replace(tooManyNewLine, "\n")
-        .split("\n");
+    const inputLines = text.replace(tooManyNewLine, "\n").split("\n");
 
     console.log(inputLines);
 };
