68b72e34ce555fcfe00a5157c155cdb9b65a698b
kangyeop <lky5697@naver.com>
2020-12-07 21:49:47 +0900

    feat: Make Ul line & Ol line

--- src/parse.js
+++ src/parse.js
@@ -12,6 +12,43 @@ export const parseBlock = (line) => {
 
 export const tokenize = (lines) => {
     let tokens = lines.map((line) => parseBlock(line));
+    tokens = tokens.map((token, i, arr) => {
+        if (
+            token.name === "OrderedList" &&
+            (!i || (i - 1 >= 0 && arr[i - 1].name !== "OrderedList"))
+        ) {
+            const olStart = "<ol>";
+            token.tag = olStart + token.tag;
+        }
+
+        if (
+            token.name === "OrderedList" &&
+            i + 1 < lines.length &&
+            arr[i + 1].name !== "OrderedList"
+        ) {
+            const olEnd = "</ol>";
+            token.tag = token.tag + olEnd;
+        }
+
+        if (
+            token.name === "UnorderedList" &&
+            (!i || (i - 1 >= 0 && arr[i - 1].name !== "UnorderedList"))
+        ) {
+            const ulStart = "<ul>";
+            token.tag = ulStart + token.tag;
+        }
+
+        if (
+            i + 1 < lines.length &&
+            token.name === "UnorderedList" &&
+            arr[i + 1].name !== "UnorderedList"
+        ) {
+            const ulEnd = "</ul>";
+            token.tag = token.tag + ulEnd;
+        }
+
+        return token;
+    });
 
     return tokens;
 };
