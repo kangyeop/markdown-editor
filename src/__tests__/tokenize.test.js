a9c188569b1dc923c6219440987bf1be6c16b9ff
kangyeop <lky5697@naver.com>
2020-12-22 23:20:25 +0900

    fix: Remove normalize function

--- src/__tests__/tokenize.test.js
+++ src/__tests__/tokenize.test.js
@@ -1,16 +1,15 @@
 import { tokenize } from "../tokenize";
-import { normalize } from "../normalize";
 
 test("Tokenize Ordered List", () => {
-    let text = `1. Test1\n2. Test2\n3. Test3\n4. Test4\n`;
+    let text = ["1. Test1", "2. Test2", "3. Test3", "4. Test4", ""];
 
-    let tokens = tokenize(normalize(text));
+    let tokens = tokenize(text);
     expect(tokens[0].tag).toEqual("<ol><li>Test1</li>");
     expect(tokens[3].tag).toEqual("<li>Test4</li></ol>");
 
-    text = `1. Test1\n2. Test2\n\n3. Test3\n4. Test4\n`;
+    text = ["1. Test1", "2. Test2", "", "3. Test3", "4. Test4", ""];
 
-    tokens = tokenize(normalize(text));
+    tokens = tokenize(text);
 
     expect(tokens[0].tag).toEqual("<ol><li>Test1</li>");
     expect(tokens[1].tag).toEqual("<li>Test2</li></ol>");
@@ -19,15 +18,15 @@ test("Tokenize Ordered List", () => {
 });
 
 test("Tokenize Unordered List", () => {
-    let text = `- Test1\n- Test2\n- Test3\n- Test4\n`;
+    let text = ["- Test1", "- Test2", "- Test3", "- Test4", ""];
 
-    let tokens = tokenize(normalize(text));
+    let tokens = tokenize(text);
     expect(tokens[0].tag).toEqual("<ul><li>Test1</li>");
     expect(tokens[3].tag).toEqual("<li>Test4</li></ul>");
 
-    text = `- Test1\n- Test2\n\n- Test3\n- Test4\n`;
+    text = ["- Test1", "- Test2", "", "- Test3", "- Test4", ""];
 
-    tokens = tokenize(normalize(text));
+    tokens = tokenize(text);
     expect(tokens[0].tag).toEqual("<ul><li>Test1</li>");
     expect(tokens[1].tag).toEqual("<li>Test2</li></ul>");
     expect(tokens[3].tag).toEqual("<ul><li>Test3</li>");
@@ -35,9 +34,9 @@ test("Tokenize Unordered List", () => {
 });
 
 test("Tokenize Unordered List with some block rules", () => {
-    let text = `- **Test1**\n- ~~Test2~~\n- *Test3*\n- Test4\n`;
+    let text = ["- **Test1**", "- ~~Test2~~", "- *Test3*", "- Test4", ""];
 
-    let tokens = tokenize(normalize(text));
+    let tokens = tokenize(text);
     expect(tokens[0].tag).toEqual("<ul><li><strong>Test1</strong></li>");
     expect(tokens[1].tag).toEqual("<li><strike>Test2</strike></li>");
     expect(tokens[2].tag).toEqual("<li><em>Test3</em></li>");
